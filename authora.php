cc68d0fb29a205754c0b4b9d067ee58133b0d36a
Rayiumir <swordart2050@gmail.com>
2025-03-13 03:08:30 +0330

    Changed const to define

--- authora.php
+++ authora.php
@@ -6,20 +6,20 @@
 
 defined('ABSPATH') || exit;
 
-const AUTHORA_LOGIN_VERSION = '1.0.0';
+define (AUTHORA_LOGIN_VERSION = '1.0.0');
 
 define( 'AUTHORA_LOGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'AUTHORA_LOGIN_PATH', plugin_dir_path(__FILE__) );
 
-const AUTHORA_LOGIN_CSS = AUTHORA_LOGIN_URL . 'css/';
-const AUTHORA_LOGIN_JS = AUTHORA_LOGIN_URL . 'js/';
-const AUTHORA_LOGIN_IMAGES = AUTHORA_LOGIN_URL . 'img/';
+define (AUTHORA_LOGIN_CSS = AUTHORA_LOGIN_URL . 'css/');
+define (AUTHORA_LOGIN_JS = AUTHORA_LOGIN_URL . 'js/');
+define (AUTHORA_LOGIN_IMAGES = AUTHORA_LOGIN_URL . 'img/');
 
 
-const AUTHORA_LOGIN_PUBLIC = AUTHORA_LOGIN_PATH . 'public/';
-const AUTHORA_LOGIN_VIEW = AUTHORA_LOGIN_PATH . 'view/';
-const AUTHORA_LOGIN_INC = AUTHORA_LOGIN_PATH . 'inc/';
-const AUTHORA_LOGIN_ADMIN = AUTHORA_LOGIN_PATH . 'admin/';
+define (AUTHORA_LOGIN_PUBLIC = AUTHORA_LOGIN_PATH . 'public/');
+define (AUTHORA_LOGIN_VIEW = AUTHORA_LOGIN_PATH . 'view/');
+define (AUTHORA_LOGIN_INC = AUTHORA_LOGIN_PATH . 'inc/');
+define (AUTHORA_LOGIN_ADMIN = AUTHORA_LOGIN_PATH . 'admin/');
 
 // Calling Files
 
